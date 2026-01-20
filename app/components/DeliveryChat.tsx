80b48f99af6a3035d3dd9b0ca9be591c46a37e0b
Duy Khang <nguyenhoangduykhang0607@gmail.com>
2026-01-20 17:50:22 +0700

    update

--- app/components/DeliveryChat.tsx
+++ app/components/DeliveryChat.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Send } from 'lucide-react';
+import { Box, Loader2, Send } from 'lucide-react';
 import mongoose from 'mongoose';
 import { useEffect, useState } from 'react';
 import { getSocket } from '../lib/socket';
@@ -17,51 +17,63 @@ type IProps = {
 const DeliveryChat = ({ orderId, deliveryBoyId }: IProps) => {
     const [newMessage, setNewMessage] = useState('');
     const [message, setMessage] = useState<IMessage[]>([]);
+    const [loading, setLoading] = useState(false);
 
 
     const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        
-        if (!newMessage.trim()) return;
+        try {
+            setLoading(true);
+            e.preventDefault();
+            if (!newMessage.trim()) return;
 
-        const socket = getSocket()
-        const messageData = {
-            roomId: orderId.toString(),
-            text: newMessage,
-            senderId: deliveryBoyId.toString(),
-            time: new Date().toLocaleDateString([], {
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-                hour12: false,
-                day: '2-digit',
-                month: '2-digit',
-                year: 'numeric',
-            }),
+            const socket = getSocket()
+            const messageData = {
+                roomId: orderId.toString(),
+                text: newMessage,
+                senderId: deliveryBoyId.toString(),
+                time: new Date().toLocaleDateString([], {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: false,
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                }),
+            }
+
+            // CHỈ EMIT message, KHÔNG listen ở đây
+            socket?.emit("send-message", messageData)
+            setNewMessage('')
+        } catch (error) {
+            console.error('Error sending message:', error)
+            setLoading(false);
+        } finally {
+            setLoading(false);
         }
-        
-        // CHỈ EMIT message, KHÔNG listen ở đây
-        socket?.emit("send-message", messageData)
-        setNewMessage('')
     }
 
     const fetchMessages = async () => {
         if (!orderId) return;
-        
+
         try {
+            setLoading(true);
             const res = await axios.post(`/api/chat/messages`, { roomId: orderId.toString() })
             if (res?.data?.success) {
                 setMessage(res?.data?.messages)
             }
         } catch (error: any) {
             console.error('Error fetching messages:', error?.response?.data?.message || error?.message || 'Unknown error')
+            setLoading(false);
+        } finally {
+            setLoading(false);
         }
     }
 
     // Join room & Listen for incoming messages
     useEffect(() => {
         if (!orderId) return;
-        
+
         const socket = getSocket()
         socket?.emit("join-room", { roomId: orderId?.toString() })
 
@@ -87,33 +99,52 @@ const DeliveryChat = ({ orderId, deliveryBoyId }: IProps) => {
 
 
     return (
-        <div className='w-full rounded-md shadow-md border border-gray-300 p-4 overflow-hidden min-h-[300px] mt-5'>
-            <div className='w-full border border-gray-500 mb-4'></div>
+        <div className='w-full rounded-md shadow-md border border-gray-300 p-4 overflow-hidden min-h-[300px] mt-5 relative'>
+            {loading ? (
+                <motion.div
+                    initial={{ y: 40, opacity: 0 }}
+                    animate={{ y: [0, -10, 0], opacity: 1 }}
+                    transition={{
+                        delay: 0.2,
+                        duration: 3,
+                        repeat: Infinity,
+                        ease: "easeInOut"
+                    }}
+                    className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+                >
+                    <Loader2 className='w-10 h-10 text-green-700 mb-5 animate-spin' />
+                </motion.div>
+            ) : (
+                <>
+                    <div className='w-full border border-gray-500 mb-4'></div>
 
-            <div className='w-full h-full flex flex-row items-center justify-center gap-2'>
-                <form className='w-full flex flex-row items-center justify-center gap-2' onSubmit={sendMessage}>
-                    <input type="text" placeholder='Your message' className='w-full p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 transition-all duration-300' value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
-                    <button disabled={newMessage.length === 0} className={`w-auto text-white rounded-md p-2 cursor-pointer transition-all duration-300 hover:bg-green-800 ${newMessage.length > 0 ? 'bg-green-700' : 'bg-gray-500'}`}>
-                        <Send className='w-5 h-5' />
-                    </button>
-                </form>
-            </div>
+                    <div className='w-full h-full flex flex-row items-center justify-center gap-2'>
+                        <form className='w-full flex flex-row items-center justify-center gap-2' onSubmit={sendMessage}>
+                            <input type="text" placeholder='Your message' className='w-full p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 transition-all duration-300' value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
+                            <button disabled={newMessage.length === 0} className={`w-auto text-white rounded-md p-2 cursor-pointer transition-all duration-300 hover:bg-green-800 ${newMessage.length > 0 ? 'bg-green-700' : 'bg-gray-500'}`}>
+                                <Send className='w-5 h-5' />
+                            </button>
+                        </form>
+                    </div>
 
-            <div className='flex-1 overflow-y-auto mt-2 space-y-3 h-full md:max-h-[200px]'>
-                <AnimatePresence mode='wait'>
-                    {message?.map((item, index) => (
-                        <motion.div
-                            key={index} initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 10 }} transition={{ duration: 0.4 }}
-                            className={`flex flex-col mt-2 space-y-2 ${item?.senderId.toString() === deliveryBoyId.toString() ? 'items-end' : 'items-start'}`}>
-                            <div className={`px-4 py-2 max-w-[80%] ${item?.senderId.toString() === deliveryBoyId.toString() ? 'bg-green-500' : 'bg-gray-500'} rounded-md shadow-md text-left ${item?.senderId.toString() === deliveryBoyId.toString() ? 'text-right' : 'text-left'}`}>
-                                <p className='text-sm text-white font-semibold'>{item?.text}</p>
-                            </div>
-                            <p className='text-xs text-gray-500 text-right'>{item?.time}</p>
-                        </motion.div>
-                    ))}
-                </AnimatePresence>
-            </div>
-        </div>
+                    <div className='w-full max-h-[500px] overflow-y-auto mt-2 space-y-3 md:max-h-[200px]'>
+                        <AnimatePresence mode='wait'>
+                            {message?.map((item, index) => (
+                                <motion.div
+                                    key={index} initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 10 }} transition={{ duration: 0.4 }}
+                                    className={`flex flex-col mt-2 space-y-2 ${item?.senderId.toString() === deliveryBoyId.toString() ? 'items-end' : 'items-start'}`}>
+                                    <div className={`px-4 py-2 max-w-[80%] ${item?.senderId.toString() === deliveryBoyId.toString() ? 'bg-green-500' : 'bg-gray-500'} rounded-md shadow-md text-left ${item?.senderId.toString() === deliveryBoyId.toString() ? 'text-right' : 'text-left'}`}>
+                                        <p className='text-sm text-white font-semibold'>{item?.text}</p>
+                                    </div>
+                                    <p className='text-xs text-gray-500 text-right'>{item?.time}</p>
+                                </motion.div>
+                            ))}
+                        </AnimatePresence>
+                    </div>
+                </>
+            )}
+
+        </div >
     )
 }
 
--- app/user/track-order/[orderId]/page.tsx
+++ app/user/track-order/[orderId]/page.tsx
@@ -7,7 +7,7 @@ import { IOrder } from "@/app/models/orders.model"
 import { RootState } from "@/app/redux/store"
 import { useSelector } from "react-redux"
 import { motion } from "framer-motion"
-import { ArrowLeft } from "lucide-react"
+import { ArrowLeft, Box } from "lucide-react"
 import { useRouter } from "next/navigation"
 import LiveMap from "@/app/components/LiveMap"
 import { getSocket } from "@/app/lib/socket"
@@ -72,31 +72,53 @@ const TrackOrder = () => {
   }, [userData?._id])
   return (
     <div className='w-[90%] md:w-[80%] mx-auto mt-5 space-y-5'>
-      <div className='flex flex-row items-center justify-start gap-4 border border-gray-300 rounded-md p-4 shadow-md overflow-hidden w-full h-full'>
-        <motion.button
-          onClick={() => router.back()}
-          whileTap={{ scale: 0.97 }}
-          whileHover={{ scale: 1.06 }}
-          className='flex items-center gap-2 text-green-700 hover:text-green-800 font-semibold cursor-pointer bg-white shadow-lg p-2 rounded-xl'>
-          <ArrowLeft className='w-5 h-5' />
-          <span className='hidden md:flex font-semibold tracking-wide'>Back to home</span>
-        </motion.button>
+      {loading ? (
+        <motion.div
+          initial={{ y: 40, opacity: 0 }}
+          animate={{ y: [0, -10, 0], opacity: 1 }}
+          transition={{
+            delay: 0.2,
+            duration: 3,
+            repeat: Infinity,
+            ease: "easeInOut"
+          }}
+          className='flex flex-col items-center justify-center min-h-screen'
+        >
+          <Box className='w-25 h-25 md:w-50 md:h-50 text-green-700 mb-5' />
+        </motion.div>
+      ) : (
+        <>
+          {/* Titiel */}
+          <div className='flex flex-row items-center justify-start gap-4 border border-gray-300 rounded-md p-4 shadow-md overflow-hidden w-full h-full'>
+            <motion.button
+              onClick={() => router.back()}
+              whileTap={{ scale: 0.97 }}
+              whileHover={{ scale: 1.06 }}
+              className='flex items-center gap-2 text-green-700 hover:text-green-800 font-semibold cursor-pointer bg-white shadow-lg p-2 rounded-xl'>
+              <ArrowLeft className='w-5 h-5' />
+              <span className='hidden md:flex font-semibold tracking-wide'>Back to home</span>
+            </motion.button>
 
-        <div className='flex flex-col gap-2'>
-          <h1 className='text-green-700 font-extrabold text-2xl md:text-3xl tracking-wide'>Track order</h1>
-          <p className='mb-4'>Order: #{order?._id?.toString()?.slice(-6)} - <span className="text-green-700 font-semibold">{order?.status}</span></p>
-        </div>
-      </div>
+            <div className='flex flex-col gap-2'>
+              <h1 className='text-green-700 font-extrabold text-2xl md:text-3xl tracking-wide'>Track order</h1>
+              <p className='mb-4'>Order: <span className="text-red-500 text-sm font-semibold">#{order?._id?.toString()?.slice(-6)}</span> - <span className="text-green-700 font-semibold">{order?.status}</span></p>
+            </div>
+          </div >
 
-      <div className="w-full h-full">
-        <LiveMap userLocation={userLocation} deliveryLocation={deliveryBoyLocation} />
-      </div>
+          {/* Map */}
+          <div className="w-full h-full">
+            <LiveMap userLocation={userLocation} deliveryLocation={deliveryBoyLocation} />
+          </div>
 
-      {/* Chat */}
-      {order?._id && userData?._id && (
-        <DeliveryChat orderId={order._id} deliveryBoyId={userData._id} />
+          {/* Chat */}
+          {
+            order?._id && userData?._id && (
+              <DeliveryChat orderId={order._id} deliveryBoyId={userData._id} />
+            )
+          }
+        </>
       )}
-    </div>
+    </div >
   )
 }
 
